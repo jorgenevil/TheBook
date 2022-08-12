e5adce6ee15806984fe93aa297d0a2bfc7056d37
Jørgen Evil Ekvoll <jorgenekvoll@gmail.com>
2022-08-12 10:41:44 +0200

    with picture

--- src/layers/layers.ts
+++ src/layers/layers.ts
@@ -1,10 +1,11 @@
 import * as layer_1_props from "./layer_1_props.json";
+// import * as layer_2_props from "./layer_2_props.json";
 import * as objects from "../json/objects.json";
 
 export const layers = [
   {
     geo_props_input: layer_1_props,
-    fill_geometry: objects["002"]
+    fill_geometry: objects["001"]
   },
   // {
   //   geo_props_input: layer_2_props,
